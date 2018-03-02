bdee3e4bbd8b3565b1438bfe0fe3bbc204f36773
Ara Ghazaryan <36397818+araghazaryan@users.noreply.github.com>
2018-03-02 07:56:08 +0100

    Update parser.py

--- parser.py
+++ parser.py
@@ -14,30 +14,31 @@ import pickle
 labels = ['Company', 'Model', 'Year', 'Category', 'User_topic', 'Name_of_file', 'Data']
 data = []
 listOfWords = []
+i = 0
 
-for dirpath, dirs, files in os.walk("./carcomplaints/"):
+for dirpath, dirs, files in os.walk("./carcomplaints2/"):
     if not dirs:
         paths = dirpath.split('\\')
-        #print(paths[0].split('/')[-1]) #company
-        #print(paths[1]) #Model
-        #print(paths[2]) #year
-        #print(paths[3]) # Category
-        #print(paths[4]) #user_topic
+        i=i+1
+        if not i % 100:
+            print (i)
         for file in files:
-            #print(file[:-4])
             fname = os.path.join(dirpath,file)
             with open(fname, encoding='utf-8') as myfile:
                 text = myfile.read()
             data.append(((paths[0].split('/')[-1], *paths[1:], file, text)))
-            #print((paths[0].split('/')[-1], *paths[1:], file, text))
-            # Here we split the text, lowercase it, strip from punctuaiton 
             d = text.split("\n")
+            text = d[1]
             words = text.split()
             for word in words:
-                currentWord = word.lower()
+                currentWord = word.lower() # lowercase the word
+                # remove non-char begining of word 
+                while len(currentWord)>0 and (ord(currentWord[0])<97 or ord(currentWord[0])>122):
+                    currentWord=currentWord[1:]    
+                # remove non-char ending of word 
                 while len(currentWord)>0 and (ord(currentWord[-1])<97 or ord(currentWord[-1])>122):
                     currentWord=currentWord[:-1]
-                if len(currentWord)>0:
+                if len(currentWord)>0 and currentWord != 'updated':
                     listOfWords.append(currentWord)
                 
 with open('file.txt', 'w') as file:
@@ -45,8 +46,7 @@ with open('file.txt', 'w') as file:
 
 df = pd.DataFrame(data, columns=labels)
 df.head()
-print(listOfWords)
-
+#print(listOfWords)
 df.to_csv('Data.csv')
 
-pickle.dump(listOfWords, open( "listofwords.p", "wb" ))  
+pickle.dump(listOfWords, open( "listofwords.p", "wb" )) 
